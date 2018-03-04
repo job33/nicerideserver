ba3399ebf21b772c00b3328a5d36406faf6e6a49
job33 <job33@hi.is>
2018-03-04 15:24:24 +0000

    post+getAll virkar

---
+++ api.js
@@ -0,0 +1,75 @@
+const express = require('express');
+
+const {
+  create,
+  readAll,
+  readOne,
+  update,
+  del,
+} = require('./rides');
+
+const router = express.Router();
+
+function catchErrors(fn) {
+  return (req, res, next) => fn(req, res, next).catch(next);
+}
+
+/* todo útfæra api */
+
+router.get('/', function(req, res, next) {
+  readAll().then(function(data) {
+    res.send(data);
+  });
+});
+
+router.post('/', function(req, res, next) {
+  create(req.body).then(function(data) {
+    let skil = {
+      id: data[0].id,
+      rideFrom: req.body.rideFrom,
+      rideTo: req.body.rideTo,
+      date: req.body.date,
+      depTime: req.body.depTime,
+      seatsAvailable: req.body.seatsAvailable,
+      cost: req.body.cost
+    };
+    res.status(201).send(skil);
+  });
+});
+
+router.get('/:rideFrom:rideTo', function(req, res, next) {
+  readOne(req.body.rideFrom, req.body.rideTo).then(function(data) {
+    res.send(data);
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+  update(req.params.id, req.body).then(function(data) {
+    if(data === 1) {
+      let skil = {
+        id: data[0].id,
+        rideFrom: req.body.rideFrom,
+        rideTo: req.body.rideTo,
+        date: req.body.date,
+        depTime: req.body.depTime,
+        seatsAvailable: req.body.seatsAvailable,
+        cost: req.body.cost
+      };
+      res.status(201).send(skil);
+    } else {
+      res.status(404).send("error: not found");
+    }
+  });
+});
+
+router.delete('/:date', function(req, res, next) {
+  del(req.body.date).then(function(data) {
+    if(data === 1) {
+      res.status(204).send();
+    } else {
+      res.status(404).send("error: not found");
+    }
+  });
+});
+
+module.exports = router;
---
+++ rides.js
@@ -0,0 +1,139 @@
+const { Client } = require('pg');
+
+const connectionString = process.env.DATABASE_URL;
+
+/**
+ * Create a ride asynchronously.
+ *
+ * @param {Object} note - ride to create
+ * @param {string} note.title - Title of note
+ * @param {string} note.text - Text of note
+ * @param {string} note.datetime - Datetime of note
+ *
+ * @returns {Promise} Promise representing the object result of creating the note
+ */
+async function create({ rideFrom, rideTo, date, depTime, seatsAvailable, cost } = {}) {
+  /* todo útfæra */
+  const client = new Client({ connectionString });
+
+  await client.connect();
+
+  const query = 'INSERT INTO rides(rideFrom, rideTo, date, depTime, seatsAvailable, cost) VALUES($1, $2, $3, $4, $5, $6) RETURNING id';
+  const values = [rideFrom, rideTo, date, depTime, seatsAvailable, cost];
+
+  try {
+    const result = await client.query(query, values);
+    return result.rows;
+  } catch (err) {
+    throw err;
+  } finally {
+    await client.end();
+  }
+}
+
+/**
+ * Read all notes.
+ *
+ * @returns {Promise} Promise representing an array of all note objects
+ */
+async function readAll() {
+  /* todo útfæra */
+  const client = new Client({ connectionString });
+  await client.connect();
+
+  try {
+    const result = await client.query('SELECT * FROM rides');
+    return result.rows;
+  } catch (err) {
+    console.error('Error selecting from data');
+    throw err;
+  } finally {
+    await client.end();
+  }
+}
+
+/**
+ * Read a single note.
+ *
+ * @param {number} id - Id of note
+ *
+ * @returns {Promise} Promise representing the note object or null if not found
+ */
+async function readOne(rideFrom, rideTo) {
+  /* todo útfæra */
+  const client = new Client({ connectionString });
+  await client.connect();
+
+  try {
+    const result = await client.query('SELECT * FROM rides WHERE (rideFrom = ($1) AND rideTO = ($2)', [rideFrom, rideTo]);
+    return result.rows;
+  } catch (err) {
+    console.error('Error selecting from data');
+    throw err;
+  } finally {
+    await client.end();
+  }
+}
+
+/**
+ * Update a note asynchronously.
+ *
+ * @param {number} id - Id of note to update
+ * @param {Object} note - Note to create
+ * @param {string} note.title - Title of note
+ * @param {string} note.text - Text of note
+ * @param {string} note.datetime - Datetime of note
+ *
+ * @returns {Promise} Promise representing the object result of creating the note
+ */
+async function update(id, { rideFrom, rideTo, date, depTime, seatsAvailable, cost } = {}) {
+  /* todo útfæra */
+  const client = new Client({ connectionString });
+
+  await client.connect();
+
+  const query = 'UPDATE rides SET rideFrom = ($7), rideTo = ($6), date = ($5), depTime = ($4), seatsAvailable = ($3), cost = ($2), WHERE id = ($1)';
+  const values = [id, cost, seatsAvailable, depTime, date, rideTo, rideFrom];
+
+  try {
+    const result = await client.query(query, values);
+    return result.rowCount;
+  } catch (err) {
+    throw err;
+  } finally {
+    await client.end();
+  }
+}
+
+/**
+ * Delete a note asynchronously.
+ *
+ * @param {number} id - Id of note to delete
+ *
+ * @returns {Promise} Promise representing the boolean result of creating the note
+ */
+async function del(date) {
+  const client = new Client({ connectionString });
+
+  await client.connect();
+
+  const query = 'DELETE FROM rides WHERE date = ($1)';
+  const values = [date];
+
+  try {
+    const result = await client.query(query, values);
+    return result.rowCount;
+  } catch (err) {
+    throw err;
+  } finally {
+    await client.end();
+  }
+}
+
+module.exports = {
+  create,
+  readAll,
+  readOne,
+  update,
+  del,
+};
